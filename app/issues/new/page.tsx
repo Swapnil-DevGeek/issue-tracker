c3c4950f136667824afb6e83430c675a9c4915be
Swapnil-DevGeek <f20220181@goa.bits-pilani.ac.in>
2024-04-11 23:58:30 +0530

    handle errors

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Button, TextField } from '@radix-ui/themes'
+import { Button, Callout, TextField } from '@radix-ui/themes'
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
 import { useState } from 'react';
@@ -11,6 +11,8 @@ const NewIssuePage = () => {
     const router = useRouter();
     const [title,setTitle] = useState("");
     const [description,setDescription] = useState("");
+
+    const [error,setError] = useState('');
      
     const issue = {
         title,
@@ -18,22 +20,33 @@ const NewIssuePage = () => {
     }
 
     async function handleSubmit() {
-        await axios.post('/api/issues',issue);
-        router.push('/issues');
+        try {
+            await axios.post('/api/issues',issue);
+            router.push('/issues');
+        } catch (error) {
+            setError('An unexpected error occured!')
+        }
     }
 
   return (
-  
-        <div className='max-w-lg space-y-3'>
-            <TextField.Root placeholder="Title" size='2' 
-            onChange={(e)=>setTitle(e.target.value)} />
-            
-            <SimpleMDE placeholder="Description"
-             onChange={(e)=>setDescription(e)}/>
-            <Button onClick={handleSubmit}
-            className='cursor-pointer'>Submit New Issue</Button>
-        </div>
-   
+        <>
+            <div className='max-w-lg mb-5'>
+                {error && <Callout.Root color='red' >
+                        <Callout.Text>
+                            {error}
+                        </Callout.Text>
+                    </Callout.Root>}
+            </div>
+            <div className='max-w-lg space-y-3'>
+                <TextField.Root placeholder="Title" size='2' 
+                onChange={(e)=>setTitle(e.target.value)} />
+                
+                <SimpleMDE placeholder="Description"
+                onChange={(e)=>setDescription(e)}/>
+                <Button onClick={handleSubmit}
+                className='cursor-pointer'>Submit New Issue</Button>
+            </div>
+        </>
   )
 }
 
